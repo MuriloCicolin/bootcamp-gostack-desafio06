4914a219ca116d92455f8071ef0d170ab8724421
MuriloCicolin <mu_cicolin@hotmail.com>
2020-03-20 20:59:26 -0300

    Estilizando Aplicaçao

---
+++ App.js
@@ -0,0 +1,22 @@
+import 'react-native-gesture-handler';
+import './src/config/ReactotronConfig';
+
+import React from 'react';
+import { StatusBar } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+
+import Routes from './src/routes';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <>
+        <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+        <Routes />
+      </>
+    </NavigationContainer>
+  )
+}
+
+
