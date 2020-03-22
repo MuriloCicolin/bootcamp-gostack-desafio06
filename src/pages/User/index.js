34d7035780abfc566d98931921723d78de8062ec
MuriloCicolin <mu_cicolin@hotmail.com>
2020-03-21 22:12:23 -0300

    Buscando dados na API

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { Component} from 'react';
 import { View } from 'react-native';
+import api from '../../services/api';
+import PropTypes from 'prop-types';
 
 // import { Container } from './styles';
 
-export default function User({props}) {
-  console.tron.log(props);
-  return <View />;
+export default class User extends Component  {
+  static propTypes = {
+    navigation: PropTypes.shape({
+      route: PropTypes.object,
+    }).isRequired
+  }
+
+  state = {
+    stars: [],
+  }
+
+  async componentDidMount() {
+    const { route } = this.props;
+    const { user } = route.params;
+
+    const response = await api.get(`/users/${user.login}/starred`);
+
+    this.setState({ stars: response.data});
+  }
+
+
+
+  render() {
+    return (
+      <View />
+
+    )
+  }
 }
