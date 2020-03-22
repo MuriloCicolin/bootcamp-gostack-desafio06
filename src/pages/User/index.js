2e869dd142c074f9f4808a1b75fb673d53119c92
MuriloCicolin <mu_cicolin@hotmail.com>
2020-03-22 00:25:50 -0300

    Listando favoritos

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,9 +1,8 @@
 import React, { Component} from 'react';
-import { View } from 'react-native';
 import api from '../../services/api';
 import PropTypes from 'prop-types';
 
-// import { Container } from './styles';
+import { Container, Header, Avatar, Name, Bio, Stars, Starred, OwnerAvatar, Info, Title, Author } from './styles';
 
 export default class User extends Component  {
   static propTypes = {
@@ -28,9 +27,32 @@ export default class User extends Component  {
 
 
   render() {
-    return (
-      <View />
+    const { route } = this.props;
+    const { user } = route.params;
+    const { stars } = this.state;
 
+    return (
+      <Container>
+        <Header>
+          <Avatar source={{ uri: user.avatar}} />
+          <Name>{user.name}</Name>
+          <Bio>{user.bio}</Bio>
+        </Header>
+
+        <Stars
+          data={stars}
+          keyExtractor={ star => String(star.id)}
+          renderItem={({ item }) => (
+            <Starred>
+              <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+              <Info>
+                <Title>{item.name}</Title>
+                <Author>{item.owner.login}</Author>
+              </Info>
+            </Starred>
+          )}
+        />
+      </Container>
     )
   }
 }
